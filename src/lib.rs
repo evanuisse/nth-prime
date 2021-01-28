69b987367990fcca268ec879d991c4717034d44d
Evan <evanpaul@Evies-MacBook.local>
2021-01-28 01:23:21 -0700

    nth-prime

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+fn is_prime(n: u32) -> bool {
+    for i in 2..(n - 1) {
+        if n % i == 0 {
+            return false
+        }
+    }
+
+    true
+}
+
+pub fn nth(n: u32) -> u32 {
+    let mut count: u32 = 0;
+    let mut i = 2;
+    while count != (n + 1) {
+     if is_prime(i) {
+      count += 1;
+     }
+    i += 1;
+    }
+   i - 1
+}
